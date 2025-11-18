50dfdd1169e8ef9de2b1230480ab292bfcdd4075
HARITHAVK <HARITHAVK@users.noreply.github.com>
2025-11-18 14:47:18 +0530

    Update Contracts.php

--- application/controllers/admin/Contracts.php
+++ application/controllers/admin/Contracts.php
@@ -203,7 +203,7 @@ class Contracts extends AdminController
         $this->load->model('currencies_model');
         $data['base_currency'] = $this->currencies_model->get_base_currency();
         $data['types']         = $this->contracts_model->get_contract_types();
-		//$data['clients']		=$this->clients_model->get('',['tblclients.active'=>1]);
+		$data['clients']		=$this->clients_model->get('',['tblclients.active'=>1]);
 		
 		$data['statuses']  = $this->contracts_model-> get_contract_status();
 		$data['project_members'] = $this->contracts_model->get_contract_members($id);
